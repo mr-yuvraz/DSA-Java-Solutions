565c4c396a06103866dfdc480c4092c3f57101b5
mr-yuvraz <yuvraz.skill@gmail.com>
2025-03-29 16:36:21 +0530

    Knapsack.java

---
+++ DynamicProgramming/Knapsack.java
@@ -0,0 +1,27 @@
+public class KnapsackDP {
+    // Function to solve 0/1 Knapsack using Dynamic Programming
+    public static int knapsack(int W, int[] weights, int[] values, int n) {
+        int[][] dp = new int[n + 1][W + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (i == 0 || w == 0)
+                    dp[i][w] = 0;
+                else if (weights[i - 1] <= w)
+                    dp[i][w] = Math.max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+                else
+                    dp[i][w] = dp[i - 1][w];
+            }
+        }
+        return dp[n][W];
+    }
+
+    public static void main(String[] args) {
+        int[] values = {60, 100, 120};
+        int[] weights = {10, 20, 30};
+        int W = 50;
+        int n = values.length;
+        
+        System.out.println("Maximum profit: " + knapsack(W, weights, values, n));
+    }
+}
