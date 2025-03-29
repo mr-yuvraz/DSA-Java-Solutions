1fd9a7bacf4433f8778563be819a1141d8595513
mr-yuvraz <yuvraz.skill@gmail.com>
2025-03-29 16:43:47 +0530

    MergeSortedLists.java

---
+++ LinkedList/MergeSortedLists.java
@@ -0,0 +1,48 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class MergeSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(3);
+        l1.next.next = new ListNode(5);
+
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(6);
+
+        MergeSortedLists merger = new MergeSortedLists();
+        ListNode mergedHead = merger.mergeTwoLists(l1, l2);
+
+        System.out.println("Merged Sorted List:");
+        printList(mergedHead);
+    }
+}
